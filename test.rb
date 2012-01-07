eb58a1e095335ef8451b14274aa0c608b2d4d4e7
Joe Van Dyk <joe@fixieconsulting.com>
2012-01-06 20:29:04 -0800

    wip

--- test.rb
+++ test.rb
@@ -1,18 +1,28 @@
 require 'rubygems'
 require 'bundler'
 require 'sequel'
+require 'amqp'
+require 'json'
 
 DB = Sequel.connect("postgres://localhost/hashes")
 
-DB.transaction do
-  DB.execute "create table foo (id serial primary key, name text not null)"
-  DB.execute "create table foos (id serial primary key, name text not null)"
+#DB.execute "create table foo (id serial primary key, name text not null)"
+#DB.execute "create table foos (id serial primary key, name text not null)"
 
-  DB[:foo].insert(:name => 'joe')
-  DB[:foos].insert(:name => 'joe foos')
-  p DB[:foo].all
-  p DB[:foos].all
+EventMachine.run do
+  connection = AMQP.connect :host => '127.0.0.1'
 
+  channel = AMQP::Channel.new(connection)
+  queue = channel.queue('test.db.insert')
+  exchange = channel.direct('')
 
-  raise 'foo'
+  queue.subscribe do |payload|
+    puts "received message #{ payload }. Disconnecting..."
+    DB[:foo].insert(JSON.parse(payload))
+    puts DB[:foo].all
+  end
+
+  10.times do |i|
+    exchange.publish({:name => "blah #{ i }"}.to_json, :routing_key => queue.name)
+  end
 end
